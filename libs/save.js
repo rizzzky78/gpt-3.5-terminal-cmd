9d9a4c0bc145aef5a93f3b72ae7f499c46c1d7ac
Rizu <91118932+rizzzky78@users.noreply.github.com>
2023-04-26 15:49:21 +0700

    Update save.js

--- libs/save.js
+++ libs/save.js
@@ -1,26 +1,18 @@
 const chatRequest = require("./handler");
 const userPrompt = require("../data/prompt");
-
-const fs = require("fs");
+const fs = require("fs").promises;
 
 const saveChat = async () => {
-  await chatRequest(userPrompt).then(
-    async ({ status, dataToken, response }) => {
-      const stats = { status, dataToken };
-      console.info(JSON.stringify(stats, null, 2));
-
-      const userChats = [];
-      userChats.push(...userPrompt);
-      userChats.push({ role: "assistant", content: response.content });
-
-      fs.writeFileSync("./data/chat.json", JSON.stringify(userChats, null, 2));
-      fs.writeFileSync(
-        `./data/chat-response-${userChats.length - 1}.txt`,
-        response.content
-      );
-      console.info("chat has been saved!");
-    }
-  );
+  const { status, dataToken, response } = await chatRequest(userPrompt);
+  
+  console.info(JSON.stringify({ status, dataToken }, null, 2));
+  
+  const userChats = [...userPrompt, { role: "assistant", content: response.content }];
+  
+  await fs.writeFile("./data/chat.json", JSON.stringify(userChats, null, 2));
+  await fs.writeFile(`./data/chat-response-${userChats.length - 1}.txt`, response.content);
+  
+  console.info("chat has been saved!");
 };
 
 module.exports = saveChat;
